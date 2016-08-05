a3b1149fd78735a219c8d7089398a68883ec00ca
shruti111 <shrutichoksi111@gmail.com>
2016-08-05 07:22:26 +0530

    Sift playground

---
+++ Swift2Practice.playground/Contents.swift
@@ -0,0 +1,45 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello123playground"
+str.rangeOfCharacterFromSet(NSCharacterSet.)
+
+str.rangeOfCharacterFromSet(NSCharacterSet(charactersInString: ","))
+
+//let numbers:Int = 1
+//str = (str as NSString).substringFromIndex(numbers)
+
+//print(str)
+
+//var length = 42
+//
+//str += String(length)
+//
+//var shoppingList = []
+//var occupations = [:]
+//
+//let vegetable = "red pepper"
+//
+//switch (vegetable) {
+//case "celery" :
+//    print("something")
+//case let x where x.hasPrefix("red") :
+//    print("red pepper")
+//    
+//default:
+//    print("default")
+//    
+//}
+//
+//
+//var anOptional: Bool! = true
+//switch (anOptional) {
+//case true:
+//    print(anOptional)
+//case false:
+//    print(anOptional)
+//default:
+//    print(anOptional)
+//}
+
